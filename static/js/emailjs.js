50f608cba0170cb9fbd3bed0a0f2c4fc70a20381
Anthony O'Brien <aux.fuse@yahoo.com>
2021-09-12 15:19:53 +0100

    Wrapped emailjs JS code body in statement that checks for appropriate page first before running file

--- static/js/emailjs.js
+++ static/js/emailjs.js
@@ -1,15 +1,17 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const serviceID = 'service_sb29jwq';
-    const templateID = 'contact_form';
-
-    emailjs.sendForm(serviceID, templateID, this)
-        .then(function() {
-            console.log('SUCCESS!');
-            document.getElementById("contact-form").reset();
-        }, function(error) {
-            console.log('FAILED...', error);
-            document.getElementById("contact-form").reset();
-        });
-});
+if(window.location.pathname == '/contact') {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        const serviceID = 'service_sb29jwq';
+        const templateID = 'contact_form';
+    
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(function() {
+                console.log('SUCCESS!');
+                document.getElementById("contact-form").reset();
+            }, function(error) {
+                console.log('FAILED...', error);
+                document.getElementById("contact-form").reset();
+            });
+    });
+}
